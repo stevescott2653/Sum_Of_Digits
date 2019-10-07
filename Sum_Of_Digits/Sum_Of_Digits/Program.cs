510b79a45aac830f127f4b2445e27307cd6736d8
Steven Scott <stevescott2653@gmail.com>
2019-10-07 14:07:48 -0400

    Add files via upload

---
+++ Sum_Of_Digits/Sum_Of_Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sum_Of_Digits
+{
+    public class SumExample
+    
+    {
+        public static void Main(string[] args)
+        {
+            int n, sum = 0, m;
+            Console.WriteLine("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n>0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.WriteLine("Sum is = "+ sum);
+        }
+    }
+}
